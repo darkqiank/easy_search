9edd5bf36ff640290969761ed0e85f9e7b0bfe43
darkqiank <darkqiank@foxmail.com>
2024-03-30 08:06:42 +0800

    打包镜像

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# 使用官方Python运行时作为父镜像
+FROM python:3.10-slim
+
+# 设置工作目录
+WORKDIR /app
+
+# 将当前目录内容复制到位于/app中的容器中
+COPY . /app
+
+# 安装requirements.txt中指定的所有依赖
+RUN pip install --no-cache-dir -r requirements.txt
+
+# 让端口80可用于世界外部
+EXPOSE 5003
+
+# 在容器启动时运行app.py
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "5003"]
--- main.py
+++ main.py
@@ -8,13 +8,13 @@ app = FastAPI()
 
 @app.get("/search/bing/")
 async def search_bing(q: str, l: Optional[str] = 'cn-zh', m: Optional[int] = 10):
-    proxy_url = os.getenv('PROXY_URL', "socks5://127.0.0.1:7890")  # 默认值是你原来硬编码的代理路径
+    proxy_url = os.getenv('PROXY_URL', None)  # 默认值是你原来硬编码的代理路径
     res = BING(proxies=proxy_url).text(keywords=q)
     return res
 
 @app.get("/search/ddgs/")
 async def search_ddgs(q: str, l: Optional[str] = 'cn-zh', m: Optional[int] = 10):
-    proxy_url = os.getenv('PROXY_URL', "socks5://127.0.0.1:7890")  # 默认值是你原来硬编码的代理路径
+    proxy_url = os.getenv('PROXY_URL', None)  # 默认值是你原来硬编码的代理路径
     res = DDGS(proxies=proxy_url,
                 timeout=20).text(q, max_results=m , region=l,)
     return res
