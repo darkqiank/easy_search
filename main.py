74a2059522dccae5c19afb259e3e297417bac08d
darkqiank <darkqiank@foxmail.com>
2024-04-01 10:45:24 +0800

    修复bug

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ app = FastAPI()
 async def search_bing(q: str, l: Optional[str] = 'cn-zh', m: Optional[int] = 10):
     proxy_url = os.getenv('PROXY_URL', None)  # 默认值是你原来硬编码的代理路径
     try:
-        res = BING(proxies=proxy_url).text(keywords=q)
-        return res
+        with BING(proxies=proxy_url) as bing:
+            res = bing.text(keywords=q)
+            return res
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
@@ -22,8 +23,9 @@ async def search_bing(q: str, l: Optional[str] = 'cn-zh', m: Optional[int] = 10)
 async def search_ddgs(q: str, l: Optional[str] = 'cn-zh', m: Optional[int] = 10):
     proxy_url = os.getenv('PROXY_URL', None)  # 默认值是你原来硬编码的代理路径
     try:
-        res = DDGS(proxies=proxy_url,
-                    timeout=20).text(q, max_results=m , region=l,)
-        return res
+        with DDGS(proxies=proxy_url,
+                    timeout=20) as ddgs:
+            res = ddgs.text(q, max_results=m , region=l,)
+            return res
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
