10f7b984c3421f7ec80d031e34ee05c6077508a5
darkqiank <darkqiank@foxmail.com>
2024-07-25 09:32:24 +0800

    新增cdn

--- test_vt.py
+++ test_vt.py
@@ -7,17 +7,18 @@ import random
 def get_rand_vt_end_point():
     vt_end_points = ["https://www.virustotal.com/",
                      "https://vtfastlycdn.451964719.xyz/",
-                     "https://vtgcorecdn.451964719.xyz/"
+                     "https://vtgcorecdn.451964719.xyz/",
+                     "https://vtlightcdn.451964719.xyz/"
                      ]
-    weights = [0.7, 0.3, 0.1]
+    weights = [0.7, 0.1, 0.1, 0.1]
     vt_end_point = random.choices(vt_end_points, weights=weights, k=1)[0]
     return vt_end_point
 
 
-for i in range(1):
+for i in range(5):
     print(i)
     start_time = time.time()
-    with VT(proxies="socks5://100.77.221.27:7890", timeout=10, vt_end_point="https://vtfastlycdn.451964719.xyz/") as vt:
+    with VT(proxies="socks5://100.77.221.27:7890", timeout=10, vt_end_point=get_rand_vt_end_point()) as vt:
         res = vt.api(input_str='sougo.com')
         # res = vt.api(input_str='baidu.com')
         with open('vt.json', 'w', encoding='utf-8') as f:
